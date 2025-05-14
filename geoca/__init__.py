15a65e407ad462db35d5dafd9f5a5ed0d0f24ec7
Haorui-Jiang <jianghr0321@163.com>
2025-05-14 11:07:38 +0800

    Update __init__.py

--- geoca/__init__.py
+++ geoca/__init__.py
@@ -1,5 +1,8 @@
-"""Top-level package for geoca."""
+"""A Python module for CA models based on geo-raster data."""
 
 __author__ = """Haorui Jiang"""
 __email__ = "jianghr0321@163.com"
 __version__ = "0.0.11"
+
+from geoca.rasterconvert import *
+from geoca.cellularautomata import *
